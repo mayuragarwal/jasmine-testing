4fbfececbf8d287f32d3797235d89c02da7712ba
Mayur Agarwal <mayurinnovate@live.com>
2016-06-04 23:11:09 +0530

    More tests for addition

--- tests/calculator_spec.js
+++ tests/calculator_spec.js
@@ -4,16 +4,24 @@
         Calculator.current = 0;
     });
 
-    it("should have current value all times", function () {
-        expect(Calculator.current).toBeDefined();
-    });
+    describe("when adding numbers", function () {
 
-    it("should add numbers first time", function () {
-        expect(Calculator.add(5)).toEqual(5);
-    });
+        it("should have current value all times", function () {
+            expect(Calculator.current).toBeDefined();
+        });
 
-    it("should add numbers again correctly", function () {
-        expect(Calculator.add(5)).toEqual(5);
-        expect(Calculator.add(5)).toEqual(10);
-    })
+        it("should add numbers first time", function () {
+            expect(Calculator.add(5)).toEqual(5);
+        });
+
+        it("should add numbers again correctly", function () {
+            expect(Calculator.add(5)).toEqual(5);
+            expect(Calculator.add(5)).toEqual(10);
+        });
+
+        it("should add multiple numbers correctly", function () {
+            expect(Calculator.add(1, 5)).toEqual(6);
+            expect(Calculator.add(5, 10, 29)).toEqual(50);
+        });
+    });
 });
