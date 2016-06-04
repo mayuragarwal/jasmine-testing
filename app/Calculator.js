259213a8ade8556d858b85603feed5cda76f0302
Mayur Agarwal <mayurinnovate@live.com>
2016-06-04 23:17:33 +0530

    passing subtract tests

--- app/Calculator.js
+++ app/Calculator.js
@@ -1,13 +1,15 @@
 ﻿var Calculator = {
     current: 0,
 
-    add: function(number){
+    add: function (number) {
         this.current += number;
         return this.current;
     },
 
-    subtract: function(number){
-        this.current -= number;
+    subtract: function (number) {
+        for (var i = 0; i < arguments.length; i++) {
+            this.current -= arguments[i];
+        }
         return this.current;
     }
 }
