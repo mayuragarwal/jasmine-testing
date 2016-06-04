500508faf09d2be3926c91f85097d6e2539330a9
Mayur Agarwal <mayurinnovate@live.com>
2016-06-04 22:55:56 +0530

    calculator add tests

---
+++ app/Calculator.js
@@ -0,0 +1,8 @@
+﻿var Calculator = {
+    current: 0,
+
+    add: function(number){
+        this.current += number;
+        return this.current;
+    }
+}
---
+++ tests/calculator_spec.js
@@ -0,0 +1,19 @@
+﻿describe("calculator", function () {
+
+    beforeEach(function () {
+        Calculator.current = 0;
+    });
+
+    it("should have current value all times", function () {
+        expect(Calculator.current).toBeDefined();
+    });
+
+    it("should add numbers first time", function () {
+        expect(Calculator.add(5)).toEqual(5);
+    });
+
+    it("should add numbers again correctly", function () {
+        expect(Calculator.add(5)).toEqual(5);
+        expect(Calculator.add(5)).toEqual(10);
+    })
+});
