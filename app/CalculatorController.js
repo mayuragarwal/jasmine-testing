3fba3b4f0b602944fb665355b5f34763fb955d2e
Mayur Agarwal <mayurinnovate@live.com>
2016-06-05 12:25:18 +0530

    Angular calculator addition implementation with unit tests

---
+++ app/CalculatorController.js
@@ -0,0 +1,11 @@
+﻿/// <reference path="../lib/angular/angular.js" />
+
+var myApp = angular.module("Calculator", []);
+
+myApp.controller("CalculatorController", function ($scope) {
+    $scope.Current = 0;
+
+    $scope.Add = function () {
+        $scope.Current += parseFloat($scope.TextInput);
+    }
+});
---
+++ tests/Angular_Calculator_Specs.js
@@ -0,0 +1,48 @@
+﻿/// <reference path="../lib/jasmine-2.4.1/jasmine.js" />
+/// <reference path="../lib/jasmine-2.4.1/boot.js" />
+/// <reference path="../lib/angular/angular-mocks.js" />
+
+describe("Angular Calculator", function () {
+
+    //beforeEach(function () {
+    //    Calculator.current = 0;
+    //});
+
+    describe("when adding numbers", function () {
+
+        it("should have current value all times", function () {
+
+            module("Calculator");
+
+            var scope = {};
+            var calc_controller;
+
+            inject(function($controller) {
+                calc_controller = $controller("CalculatorController", { $scope: scope });
+            })
+
+            expect(scope.Current).toBeDefined();
+        });
+
+        it("should add numbers first time", function () {
+
+            module("Calculator");
+
+            var scope = {};
+            var calc_controller;
+
+            inject(function ($controller) {
+                calc_controller = $controller("CalculatorController", { $scope: scope });
+            })
+
+            scope.TextInput = "5"
+            scope.Add();
+            expect(scope.Current).toEqual(5);
+
+            scope.TextInput = "15"
+            scope.Add();
+            expect(scope.Current).toEqual(20);
+        });
+    });
+
+});
