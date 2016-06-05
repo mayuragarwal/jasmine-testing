710a425a1e5c6c7ca6fc4792d74671962ca4a31a
Mayur Agarwal <mayurinnovate@live.com>
2016-06-05 13:17:13 +0530

    Angular calculator reset functionality and its unit tests

--- app/CalculatorController.js
+++ app/CalculatorController.js
@@ -5,6 +5,11 @@ var myApp = angular.module("Calculator", []);
 myApp.controller("CalculatorController", function ($scope) {
     $scope.Current = 0;
 
+    $scope.Reset = function () {
+        $scope.Current = 0;
+        $scope.TextInput = "0";
+    }
+
     $scope.Add = function () {
         $scope.Current += parseFloat($scope.TextInput);
     }
--- tests/Angular_Calculator_Specs.js
+++ tests/Angular_Calculator_Specs.js
@@ -1,48 +1,66 @@
-﻿/// <reference path="../lib/jasmine-2.4.1/jasmine.js" />
+﻿/// <reference path="../lib/angular/angular.js" />
+/// <reference path="../lib/jasmine-2.4.1/jasmine.js" />
 /// <reference path="../lib/jasmine-2.4.1/boot.js" />
 /// <reference path="../lib/angular/angular-mocks.js" />
 
 describe("Angular Calculator", function () {
 
-    //beforeEach(function () {
-    //    Calculator.current = 0;
-    //});
+    var scope, calc_controller;
+
+    beforeEach(function () {
+
+        module("Calculator");
+
+        inject(function ($controller, $rootScope) {
+            scope = $rootScope.$new();
+            calc_controller = $controller("CalculatorController", { $scope: scope });
+        })
+
+        scope.current = 0;
+    });
 
     describe("when adding numbers", function () {
 
         it("should have current value all times", function () {
-
-            module("Calculator");
-
-            var scope = {};
-            var calc_controller;
-
-            inject(function($controller) {
-                calc_controller = $controller("CalculatorController", { $scope: scope });
-            })
 
             expect(scope.Current).toBeDefined();
         });
 
         it("should add numbers first time", function () {
 
-            module("Calculator");
-
-            var scope = {};
-            var calc_controller;
-
-            inject(function ($controller) {
-                calc_controller = $controller("CalculatorController", { $scope: scope });
-            })
-
             scope.TextInput = "5"
             scope.Add();
             expect(scope.Current).toEqual(5);
+        });
+
+        it("should add numbers again correctly", function () {
 
             scope.TextInput = "15"
             scope.Add();
-            expect(scope.Current).toEqual(20);
+            expect(scope.Current).toEqual(15);
+
+            scope.TextInput = "15.50"
+            scope.Add();
+            expect(scope.Current).toEqual(30.5);
         });
+
     });
 
+    describe("when reset calculator", function () {
+
+        it("should reset current when called", function () {
+            scope.Current = 20;
+            scope.Reset();
+            expect(scope.current).toEqual(0);
+        });
+
+        it("should reset text input and current when called", function () {
+            scope.TextInput = "5"
+            scope.Add();
+            scope.Reset();
+            expect(scope.Current).toEqual(0);
+            expect(scope.TextInput).toEqual("0");
+        });
+
+    });
 });
