61037bd8f0532a7acae14fdae33a75883bebda9c
Mayur Agarwal <mayurinnovate@live.com>
2016-06-05 10:07:09 +0530

    angular js unit testing - hello world

---
+++ app/HelloAngular.js
@@ -0,0 +1,7 @@
+﻿/// <reference path="../lib/angular/angular.js" />
+
+var myApp = angular.module("myApp", []);
+
+myApp.controller("myCtrl", function ($scope) {
+    $scope.title = "Hello World"
+});
---
+++ tests/HelloAngular_Specs.js
@@ -0,0 +1,20 @@
+﻿/// <reference path="../lib/jasmine-2.4.1/jasmine.js" />
+/// <reference path="../lib/jasmine-2.4.1/boot.js" />
+/// <reference path="../lib/angular/angular-mocks.js" />
+
+describe("Hello Angular Tests", function() {
+    describe("myCtrl title tests", function() {
+        it("should initialize title in the scope", function () {
+
+            module("myApp");
+            var scope = {};
+            var ctrl;
+
+            inject(function($controller) {
+                ctrl = $controller("myCtrl", { $scope: scope });
+            })
+            expect(scope.title).toBeDefined();
+            expect(scope.title).toEqual("Hello World");
+        })
+    })
+})
